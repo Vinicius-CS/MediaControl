91e257a20bb9a9fbe77da67f31b63f0cc1ff0efd
Vinicius-CS <vinicius--cruz@hotmail.com>
2022-03-23 23:12:38 -0300

    Starter

---
+++ MediaControl.py
@@ -0,0 +1,93 @@
+from win32con import KEYEVENTF_EXTENDEDKEY, VK_MEDIA_PLAY_PAUSE, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from cvzone.HandTrackingModule import HandDetector
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from math import hypot
+import numpy as np
+import win32api
+import cv2
+
+# Variables
+width, height = 1280, 720
+delay = 30
+delayCounter = 0
+
+# Camera settings
+cap = cv2.VideoCapture(0)
+cap.set(0, width)
+cap.set(0, height)
+
+# Hand detector
+detectorHand = HandDetector(detectionCon=0.9, maxHands=1)
+
+# Audio
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+volMin, volMax = volume.GetVolumeRange()[:2]
+
+while True:
+    # Get the image from the camera
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    # Find the hand and its landmarks
+    hands, img = detectorHand.findHands(img)
+
+    # If the hand is detected
+    if hands:
+        # List the 21 landmarks
+        lmList = hands[0]["lmList"]
+
+        # List which fingers are raised
+        fingers = detectorHand.fingersUp(hands[0])
+
+        # If the index, middle and ring fingers are raised
+        if fingers == [0, 1, 1, 1, 0]:
+            # Get the position of the fingers
+            x1 = int(np.interp(lmList[8][1], [20, width], [10, width]))
+
+            # Adjusts volume according to finger position
+            vol = np.interp(hypot(x1), [15, 220], [volMax, volMin])
+            volume.SetMasterVolumeLevel(vol, None)
+
+        # If the index and middle fingers are raised
+        if fingers == [0, 1, 1, 0, 0]:
+            # Get the distance between the index and middle fingers
+            length, info, img = detectorHand.findDistance(lmList[8], lmList[12], img)
+
+            # If the distance between the index and middle finger is greater than 70
+            if length > 25:
+                # Delay pause and play
+                delayCounter += 1
+                if delayCounter > delay:
+                    delayCounter = 0
+                    # Pause or play
+                    win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENDEDKEY, 0)
+
+        # If the thumb is up
+        if fingers == [0, 0, 0, 0, 1]:
+            # Delay for next media
+            delayCounter += 1
+            if delayCounter > delay:
+                delayCounter = 0
+                # Next media
+                win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
+
+        # If the little finger is raised
+        if fingers == [1, 0, 0, 0, 0]:
+            # Delay go back to previous media
+            delayCounter += 1
+            if delayCounter > delay:
+                delayCounter = 0
+                # Previous media
+                win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_EXTENDEDKEY, 0)
+
+    # Show camera
+    cv2.imshow("Media Control", img)
+
+    # Press anything button to close
+    if cv2.waitKey(1) > -1:
+        break
